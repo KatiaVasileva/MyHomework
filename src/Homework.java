a0f2a2278441a53fd4f7b96d07b72817bf270fcd
katiavasileva <eka.vasileva@gmail.com>
2022-12-13 18:29:20 +0500

    Initial Commit

--- src/Homework.java
+++ src/Homework.java
@@ -1,5 +1,11 @@
-public class Main {
+public class Homework {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book book = new Book("Stephen King", 100, "Book Publishing Ltd.", 2015);
+        System.out.println(book.getAuthorName() + ", " + book.getPageAmount() + " страниц, издательство "
+                + book.getPublisherName() + ", " + book.getPublishingYear() + " год издания");
+        book.setPublishingYear(2022);
+        System.out.println(book.getAuthorName() + ", " + book.getPageAmount() + " страниц, издательство "
+                + book.getPublisherName() + ", " + book.getPublishingYear() + " год издания");
     }
 }
